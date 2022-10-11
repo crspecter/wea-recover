d1716bc0adb8bb7ee57b04839cf7153c7168f04d
ran.chen <ran.chen@dmall.com>
2022-10-11 15:44:26 +0800

    1

--- parser/file_parser.go
+++ parser/file_parser.go
@@ -1,1 +1,22 @@
 package parser
+
+import (
+	"github.com/go-mysql-org/go-mysql/replication"
+)
+
+var EventChan chan (*replication.BinlogEvent)
+
+func WeaFileEventParser(event *replication.BinlogEvent) error {
+	EventChan <- event
+	return nil
+}
+
+func RunParser(file string, offset int64) error {
+	parser := replication.NewBinlogParser()
+	return parser.ParseFile(file, offset, WeaFileEventParser)
+}
+
+func GetFileEvent() *replication.BinlogEvent {
+	e := <-EventChan
+	return e
+}
