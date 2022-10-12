1c51af525f1c7b98994f0712195e8839bdf00993
ran.chen <ran.chen@dmall.com>
2022-10-12 13:59:13 +0800

    1

---
+++ mysql/operator.go
@@ -0,0 +1,112 @@
+package mysql
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-mysql-org/go-mysql/client"
+	"time"
+)
+
+var Pool *client.Pool
+
+type DBConfig struct {
+	Addr     string
+	User     string
+	Password string
+	DBName   string
+}
+
+func NewConnPool(cfg DBConfig) {
+	Pool = client.NewPool(func(format string, args ...interface{}) {
+		println(fmt.Sprint(args)) // 增加日志方法
+	}, 1, 5, 5, cfg.Addr, cfg.User, cfg.Password, cfg.DBName)
+}
+
+func GetShowCreateTableSql(tName string) (string, error) {
+	sql := ""
+	err := QueryForRow(fmt.Sprintf("show CREATE TABLE `%s`", tName), nil, &sql)
+	if err != nil {
+		return "", err
+	}
+	return sql, err
+}
+
+func Execute(stmt string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	conn, err := Pool.GetConn(ctx)
+	if err != nil {
+		return err
+	}
+	defer Pool.PutConn(conn)
+	_, err = conn.Execute(stmt)
+	return err
+}
+
+func QueryForSlice(stmt string, rslIndex int) (rsl []string, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	conn, err := Pool.GetConn(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer Pool.PutConn(conn)
+	r, err := conn.Execute(stmt)
+	if err != nil {
+		return nil, err
+	}
+	for i := 0; i < r.RowNumber(); i++ {
+		data, err := r.GetString(i, rslIndex)
+		if err != nil {
+			return nil, err
+		}
+		rsl = append(rsl, data)
+	}
+	return
+}
+
+func QueryForRow(stmt string, rslData ...interface{}) (err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	conn, err := Pool.GetConn(ctx)
+	if err != nil {
+		return err
+	}
+	defer Pool.PutConn(conn)
+
+	err = conn.Ping()
+	if err != nil {
+		return err
+	}
+	r, err := conn.Execute(stmt)
+	if err != nil {
+		return err
+	}
+	for i := 0; i < r.RowNumber(); i++ {
+		for j := 0; j < r.ColumnNumber() && j < len(rslData); j++ {
+			if rslData[j] == nil {
+				continue
+			}
+			switch v := rslData[j].(type) {
+			case *uint64:
+				*v, err = r.GetUint(i, j)
+				if err != nil {
+					return err
+				}
+			case *int64:
+				*v, err = r.GetInt(i, j)
+				if err != nil {
+					return err
+				}
+			case *string:
+				*v, err = r.GetString(i, j)
+				if err != nil {
+					return err
+				}
+			default:
+				return fmt.Errorf("只支持，nil,*uint64,*int64,*string类型")
+			}
+		}
+	}
+	return
+}
